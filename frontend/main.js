a750f6dc7512099fdab9c63b9c34a089ad176f9e
Eldar Klyuev <e.s.klyuev@gmail.com>
2023-08-17 20:20:17 +0300

    Add tooltip for roads

--- frontend/main.js
+++ frontend/main.js
@@ -7,17 +7,29 @@ import LineString from 'ol/geom/LineString.js';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { Style, Stroke } from 'ol/style';
-// import {fromLonLat} from 'ol/proj.js';
 import {useGeographic} from 'ol/proj.js';
+import Overlay from 'ol/Overlay.js';
 import axios from 'axios';
 
 useGeographic();
 
-function showRoad() {
-  axios.get('http://127.0.0.1:8000/api/v1/road/10094/').then(function (response) {
-    var coordinates = response.data;
+const toolTipElement = document.createElement('div');
+toolTipElement.className = 'tooltip';
+document.body.appendChild(toolTipElement);
 
-    const inputString = coordinates[0].coordinates;
+const tooltip = new Overlay({
+  element: toolTipElement,
+  offset: [0, 50],
+  positioning: 'bottom-center',
+})
+
+
+
+function showRoad(road_code) {
+  axios.get(`http://127.0.0.1:8000/api/v1/road/${road_code}/`).then(function (response) {
+    var data = response.data;
+
+    const inputString = data[0].coordinates;
     const trimmedString = inputString.slice(1, -1);
     const coordinatesArray = JSON.parse("[" + trimmedString + "]")
 
@@ -41,6 +53,21 @@ function showRoad() {
       style: lineStyle
     });
 
+    map.on('pointermove', (event) => {
+      const features = map.getFeaturesAtPixel(event.pixel);
+
+      if (features.length > 0) {
+        const coordinates = event.coordinate;
+
+        toolTipElement.innerHTML = `Наименование: ${data[0].name}<br/>Протяженность: ${data[0].length_km}`;
+        tooltip.setPosition(coordinates);
+        toolTipElement.style.display = 'block';
+      } else {
+        tooltip.setPosition(undefined);
+        toolTipElement.style.display = 'none'
+      }
+    });
+
     map.addLayer(vectorLayer);
   })
 }
@@ -60,19 +87,32 @@ const map = new Map({
   view: view,
 });
 
+map.addOverlay(tooltip);
+
 const sidebar = document.getElementById('sidebar');
 
-// showRoad();
-function onClick(id, callback) {
-  document.getElementById(id).addEventListener('click', callback);
+function allroads() {
+  axios.get('http://127.0.0.1:8000/api/v1/allroads/').then(function (response) {
+    const data = response.data;
+    // console.log(data[0].name);
+
+    data.forEach(element => {
+      const button = document.createElement('button');
+      button.textContent = element.name;
+      button.setAttribute('id', `button-${element.road_code}`);
+      button.addEventListener('click', () => {
+        showRoad(element.road_code);
+        view.animate({
+          center: [43.8198579971354,43.5979421994245],
+          duration: 2000,
+          zoom: 13
+        });
+      });
+      sidebar.appendChild(button);
+
+    });
+  })
 }
 
-onClick('pan-to', function () {
-  showRoad();
-  view.animate({
-    center: [43.8198579971354,43.5979421994245],
-    duration: 2000,
-    zoom: 10
-  })
-})
+allroads();
 
